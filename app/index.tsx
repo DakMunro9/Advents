646a43a22eea3b607e7fe4632c4fbc7dabcb56a9
Foxxybb <62300241+Foxxybb@users.noreply.github.com>
2023-11-16 11:00:30 -0600

    finished welcome screen

--- app/index.tsx
+++ app/index.tsx
@@ -16,7 +16,7 @@ export default function Page() {
          <Button title="Day"/> 
       </Link>
 
-      <Link href="/welcome3" asChild>
+      <Link href="/welcome" asChild>
          <Button title="Welcome"/> 
       </Link>
 
--- app/welcome.tsx
+++ app/welcome.tsx
@@ -1,34 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 import { Link } from 'expo-router';
 
-export default function Page() {
+// "Explore upcoming and nearby events"
+const FirstRoute = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+
+    <Text style={{ marginBottom: 100, }}>(image preview here)</Text>
+
+    <View>
+      <Text style={styles.mainText}>Explore Upcoming and Nearby Events</Text>
+    </View>
+    <View>
+      <Text style={styles.subText}>By ADVENTS</Text>
+    </View>
+
+  </View>
+);
+
+
+const SecondRoute = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+
+    <Text>Second Tab</Text>
+
+  </View>
+);
+
+// "The best way to plan your event for any occasion."
+const ThirdRoute = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+
+    <Text style={{ marginBottom: 100 }}>(icon here)</Text>
+
+    <View>
+      <Text style={styles.mainText}>  The best way to plan your event for any occasion.</Text>
+    </View>
+    <View>
+      <Text style={styles.subText}>ADVENTS</Text>
+    </View>
+
+
+  </View>
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+  third: ThirdRoute,
+});
+
+const TabbedView = () => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+    { key: 'third', title: 'Third' },
+  ]);
+
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: 'lightblue', height: 5 }}
+      style={{ backgroundColor: 'white', height: 5 }}
+    />
+  );
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
 
-      <Text style={{ marginTop: 100 }}>(app preview here)</Text>
+      {/* Tab view */}
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        //renderTabBar={() => null} // removes tab bar
+        renderTabBar={renderTabBar}
+        tabBarPosition='bottom'
+      />
 
-      <View style={styles.titleContainer}>
-        <Text style={styles.mainText}>Explore Upcoming and Nearby Events</Text>
-        <Text style={styles.subText}>By ADVENTS</Text>
-      </View>
-
-      <View style={styles.divider}>
-        <Text>                                </Text>
-      </View>
-
+      {/* Signup/Login view */}
       <View style={styles.loginContainer}>
 
         <View style={styles.buttonContainer}>
-          <Button 
+          <Button
             title="Create an account"
             color={"black"}
             onPress={() => alert('Signup button clicked')}
           />
         </View>
-      
+
         <View style={styles.textContainer}>
           <Text style={styles.subText}>Already have an account? </Text>
           <Link href="/" asChild>
@@ -37,29 +99,17 @@ export default function Page() {
         </View>
 
       </View>
-
     </View>
-
   );
-}
+
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textContainer: {
-    flexDirection: 'row',
-    padding: 10, // Adjust the padding as needed
-    //marginTop: 'auto',
-    alignItems: 'center',
-  },
   mainText: {
     fontSize: 50,
     fontWeight: 'bold',
     justifyContent: 'center',
+    textAlign: 'center',
     //color: 'gray',
   },
   subText: {
@@ -69,26 +119,30 @@ const styles = StyleSheet.create({
     color: 'gray',
     alignItems: 'center',
   },
+  loginContainer: {
+    marginTop: 30, // Push the view to the bottom
+    marginBottom: 50,
+    backgroundColor: 'white',
+  },
+  buttonContainer: {
+    marginTop: 20, // Adjust the padding as needed
+    marginHorizontal: 50,
+    backgroundColor: 'white',
+  },
+  textContainer: {
+    flexDirection: 'row',
+    marginTop: 20, // Adjust the padding as needed
+    //marginTop: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   linkText: {
     fontSize: 20,
     fontWeight: 'bold',
     //justifyContent: 'flex-start',
     //color: 'gray',
   },
-  buttonContainer: {
-    //width: '90%',
-  },
-  divider: {
-    height: 5,
-    backgroundColor: 'lightblue', // You can change the color of the divider
-    marginVertical: 20, // Adjust the vertical spacing above and below the divider
-  },
-  loginContainer: {
-    marginTop: 'auto', // Push the view to the bottom
-    marginBottom: 50,
-  },
-  titleContainer: {
-    marginTop: 'auto', // Push the view to the bottom
-    alignItems: 'center',
-  }
+
 });
+
+export default TabbedView;
