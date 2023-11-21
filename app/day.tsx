4d4825ee3e741d60c1a9b821e3dffae685e34c80
Foxxybb <62300241+Foxxybb@users.noreply.github.com>
2023-11-21 14:58:35 -0600

    updated calendar appearance and functions

--- app/day.tsx
+++ app/day.tsx
@@ -1,15 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { Calendar, CalendarTheme } from 'react-native-calendars';
+import { Calendar, CalendarTheme, LocaleConfig } from 'react-native-calendars';
 import Icon from 'react-native-vector-icons/Feather';
 import { Link } from 'expo-router';
 import PrimaryButton from '../components/common/PrimaryButton';
 
+LocaleConfig.locales['myConfig'] = {
+    monthNames: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ],
+    monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+};
+
+LocaleConfig.defaultLocale = 'myConfig';
+
 export default function Page() {
+
+    {/* Time Calculator */ }
+    const today = new Date(); // Get the current date
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [daysDifference, setDaysDifference] = useState('?');
+
+    const calculateDaysDifference = (end) => {
+        const startDateObject = new Date();
+        const endDateObject = new Date(end);
+
+        const timeDifference = endDateObject.getTime() - startDateObject.getTime();
+        const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+        setDaysDifference(daysDifference);
+    };
+
     const customTheme: CalendarTheme = {
-        textMonthFontSize: 22, // Set the font size for the month text
-        textDayFontSize: 20,   // Set the font size for the day text
-        textDayHeaderFontSize: 18, // Set the font size for the day header text
+        textMonthFontSize: 22,
+        textDayFontSize: 20,
+        textDayHeaderFontSize: 18,
+    };
+
+    const [selectedDate, setSelectedDate] = useState('');
+
+    const handleDayPress = (day) => {
+        setSelectedDate(day.dateString);
+        calculateDaysDifference(day.dateString);
     };
 
     return (
@@ -20,12 +67,30 @@ export default function Page() {
                 <Calendar
                     style={{
                         borderWidth: 0,
+                        width: 400,
+                        height: 300,
                     }}
                     theme={customTheme}
+                    minDate={today.toISOString().split('T')[0]} // Sets the minimum selectable date
                     // Customize calendar props as needed
-                    onDayPress={(day) => console.log('selected day', day)}
+                    //onDayPress={(day) => console.log('selected day', day)}
+                    onDayPress={handleDayPress}
+                    markingType={'custom'}
                     markedDates={{
-                        // marked dates will most likely be updated via database
+                        [selectedDate]: {
+                            customStyles: {
+                                container: {
+                                    backgroundColor: 'black',
+                                    borderRadius: 5,
+                                    marginHorizontal: 20,
+                                    elevation: 2,
+                                },
+                                text: {
+                                    color: 'white',
+                                    fontWeight: 'bold'
+                                }
+                            }
+                        },
                     }}
                 />
             </View>
@@ -34,23 +99,21 @@ export default function Page() {
                 <View style={styles.textContainer}>
                     <Icon name="info" size={30} color="gray" />
                     <Text style={styles.infoText}> These dates are available to you.</Text>
-
                 </View>
                 <View style={styles.textContainer}>
                     <Icon name="clock" size={30} color="gray" />
-                    <Text style={styles.infoText}> ? days until event.</Text>
+                    <Text style={styles.infoText}> {daysDifference} day(s) until event.</Text>
                 </View>
             </View>
 
             <View style={styles.buttonContainer}>
                 <PrimaryButton
                     title="Set day"
-                    isPressed={() => alert('Continue button clicked')}
+                    isPressed={() => alert('Set day button clicked')}
                 />
             </View>
 
         </View>
-
     );
 };
 
@@ -67,7 +130,6 @@ const styles = StyleSheet.create({
         padding: 16, // Adjust the padding as needed
         //backgroundColor: 'white', // Optional background color for the padding area
     },
-
     infoText: {
         fontSize: 18,
         fontWeight: 'bold',
@@ -75,8 +137,9 @@ const styles = StyleSheet.create({
         color: 'gray',
     },
     calendarWrapper: {
-        height: '40%',
-        width: '80%',
+        marginTop: 50,
+        //height: '50%',
+        //width: '90%',
     },
     buttonContainer: {
         marginBottom: 10, // Adjust as needed for spacing
