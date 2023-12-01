b0d3f12b47c0da1eb24ecb39e9db09df1ed65eed
Foxxybb <62300241+Foxxybb@users.noreply.github.com>
2023-12-01 15:24:22 -0600

    Update connectsocial2.tsx

--- app/connectsocial2.tsx
+++ app/connectsocial2.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Link, useRouter } from 'expo-router';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import PrimaryButton from '../components/common/PrimaryButton';
@@ -23,61 +23,123 @@ export default function Page() {
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center', }}>
+    <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', }}>
 
       <View style={{ marginVertical: 40, }}>
         <Text style={styles.mainText}>Connect your social accounts</Text>
         <Text style={styles.subText}>Enhance your experience</Text>
       </View>
 
-      {/* facebook */}
-      <View style={{ marginHorizontal: 10, marginVertical: 50, flexDirection: 'row' }}>
-        <View style={{flexDirection: 'row', marginTop: 10}}>
-          <IconFontAwesome name="facebook-official" size={30} color="black" />
-          <Text style={styles.overlayText}> Facebook.com/</Text>
+      <ScrollView>
+        {/* facebook */}
+        <View style={{ marginHorizontal: 30, marginVertical: 5, flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <IconFontAwesome name="facebook-official" size={30} color="black" />
+            <Text style={styles.overlayText}> Facebook.com/</Text>
+          </View>
+
+          <TextInput
+            style={[
+              styles.textInput,
+              selectedInput === 'textInput1' ? styles.focusedTextInput : null,
+            ]}
+            placeholder="username"
+            onChangeText={(text) => console.log(text)}
+            onFocus={() => handleFocus('textInput1')}
+            onBlur={handleBlur}
+          />
+
         </View>
 
-        <TextInput
-          style={[
-            styles.textInput,
-            selectedInput === 'textInput1' ? styles.focusedTextInput : null,
-          ]}
-          placeholder="username"
-          onChangeText={(text) => console.log(text)}
-          onFocus={() => handleFocus('textInput1')}
-          onBlur={handleBlur}
-        />
+        {/* instagram */}
+        <View style={{ marginHorizontal: 30, marginVertical: 5, flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <IconFontAwesome name="instagram" size={30} color="black" />
+            <Text style={styles.overlayText}> Instagram.com/</Text>
+          </View>
 
-      </View>
+          <TextInput
+            style={[
+              styles.textInput,
+              selectedInput === 'textInput2' ? styles.focusedTextInput : null,
+            ]}
+            placeholder="username"
+            onChangeText={(text) => console.log(text)}
+            onFocus={() => handleFocus('textInput2')}
+            onBlur={handleBlur}
+          />
 
-      {/* facebook */}
-      <View style={{ marginHorizontal: 10, marginVertical: 30, flexDirection: 'row' }}>
-        <View style={{flexDirection: 'row', marginTop: 10}}>
-          <IconFontAwesome name="facebook-official" size={30} color="black" />
-          <Text style={styles.overlayText}> Facebook.com/</Text>
         </View>
 
-        <TextInput
-          style={[
-            styles.textInput,
-            selectedInput === 'textInput1' ? styles.focusedTextInput : null,
-          ]}
-          placeholder="username"
-          onChangeText={(text) => console.log(text)}
-          onFocus={() => handleFocus('textInput1')}
-          onBlur={handleBlur}
-        />
+        {/* Twitter */}
+        <View style={{ marginLeft: 25, marginVertical: 5, flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <IconFontAwesome name="twitter" size={30} color="black" />
+            <Text style={styles.overlayText}> Twitter.com/</Text>
+          </View>
 
-      </View>
+          <TextInput
+            style={[
+              styles.textInput,
+              selectedInput === 'textInput3' ? styles.focusedTextInput : null,
+            ]}
+            placeholder="username"
+            onChangeText={(text) => console.log(text)}
+            onFocus={() => handleFocus('textInput3')}
+            onBlur={handleBlur}
+          />
 
-      
+        </View>
 
-      <View style={{ marginTop: 'auto', width: '80%', marginBottom: 20 }}>
-        <PrimaryButton
-          title="Save"
-          isPressed={() => router.push("connectsocial")}
-        />
-      </View>
+        {/* Tiktok */}
+        <View style={{ marginLeft: 20, marginVertical: 5, flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <Ionicons name="musical-note" size={30} color="black" />
+            <Text style={styles.overlayText}> Tiktok.com/</Text>
+          </View>
+
+          <TextInput
+            style={[
+              styles.textInput,
+              selectedInput === 'textInput4' ? styles.focusedTextInput : null,
+            ]}
+            placeholder="username"
+            onChangeText={(text) => console.log(text)}
+            onFocus={() => handleFocus('textInput4')}
+            onBlur={handleBlur}
+          />
+
+        </View>
+
+        {/* Pinterest */}
+        <View style={{ marginHorizontal: 23, marginVertical: 5, flexDirection: 'row' }}>
+          <View style={{ flexDirection: 'row', marginTop: 10 }}>
+            <IconFontAwesome name="pinterest" size={30} color="black" />
+            <Text style={styles.overlayText}> Pinterest.com/</Text>
+          </View>
+
+          <TextInput
+            style={[
+              styles.textInput,
+              selectedInput === 'textInput5' ? styles.focusedTextInput : null,
+            ]}
+            placeholder="username"
+            onChangeText={(text) => console.log(text)}
+            onFocus={() => handleFocus('textInput5')}
+            onBlur={handleBlur}
+          />
+
+        </View>
+      </ScrollView>
+
+
+        <View style={{ marginTop: 80, width: '80%', marginBottom: 20 }}>
+          <PrimaryButton
+            title="Save"
+            isPressed={() => router.push("connectsocial")}
+          />
+        </View>
+
 
     </View>
   );
@@ -120,7 +182,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     paddingHorizontal: 10,
     height: 50,
-    width: '50%',
+    width: '60%',
     marginBottom: 20,
   },
   focusedTextInput: {
